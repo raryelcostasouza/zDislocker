1dc0d2e7314c46dce2576894bea2c3845c3f447b
Raryel C. Souza <raryel.costa@gmail.com>
2019-04-08 00:02:09 +0700

    Change not_mounted -> unmounted

--- app/dislocker-gui.sh
+++ app/dislocker-gui.sh
@@ -102,7 +102,7 @@ function getMountedBitlockerDrives
 
 function getNotMountedBitlockerDrives
 {
-  getSelectionListBitlockerDrives "not_mounted"
+  getSelectionListBitlockerDrives "unmounted"
 }
 
 function getSelectionListBitlockerDrives
