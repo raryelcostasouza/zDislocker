65f59938ccf4c8fddd1558e76dc19bd327ed3090
Raryel C. Souza <raryel.costa@gmail.com>
2019-04-08 14:20:37 +0700

    Updated header info

--- app/util-root.sh
+++ app/util-root.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Copyright (C) 2018 Raryel C. Souza <raryel.costa at gmail.com>
+#Copyright (C) 2019 Raryel C. Souza <raryel.costa at gmail.com>
 
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
